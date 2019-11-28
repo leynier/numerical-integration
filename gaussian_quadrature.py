662c942fd4fdf5fe2791855856fbca7bd18a072b
leynier <leynier41@gmail.com>
2019-11-28 18:45:06 -0500

    Implement gaussian quadrature method

--- gaussian_quadrature.py
+++ gaussian_quadrature.py
@@ -1,2 +1,6 @@
+from roots_legendre import roots_legendre
+
+
 def gaussian_quadrature(f, a, b, n = 5):
-    pass
+    x, w = roots_legendre(n + 1)
+    return 0.5 * (b - a) * sum(w * f(0.5 * (b - a) * x + 0.5 * (b + a)))
