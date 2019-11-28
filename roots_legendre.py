a67eb4be48ef7f4fdddbd0ce68a6911466e63881
leynier <leynier41@gmail.com>
2019-11-28 18:41:40 -0500

    Implement roots legendre method

--- roots_legendre.py
+++ roots_legendre.py
@@ -1,2 +1,8 @@
+from scipy.special import legendre
+
+
 def roots_legendre(n):
-    pass
+    p = legendre(n + 1)
+    x = legendre(n).roots
+    w = 2 * (1 - x ** 2) / ((n + 1) ** 2 * p(x) ** 2)
+    return x, w
