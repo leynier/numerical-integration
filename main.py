1090f3c9a9e30bf54ced8ace04ea656314e92dfe
leynier <leynier41@gmail.com>
2019-11-28 19:16:50 -0500

    Add mock for using analytical form

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gaussian_quadrature import gaussian_quadrature
 from utils import print_error
 
 
-if __name__ == "__main__":
+def tabular_form():
     try:
         n = int(input('Insert the number of points of the function to integrate: '))
     except ValueError:
@@ -23,3 +23,20 @@ if __name__ == "__main__":
         print_error('Error: Incorrect format, the limits should be a numbers and the separator should be a comma ",".')
     spline = CubicSpline(x_points, y_points)
     print(gaussian_quadrature(spline, a, b))
+
+
+def analytical_form():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        option = int(input('Insert 1 if the function to be integrated is in the form of a table or 2 if it is in analytical form: ))
+        if option != 1 and option != 2:
+            print_error('Error: The number of option should be 1 or 2.')
+    except ValueError:
+        print_error('Error: The number of option should be a integer.')
+    if option == 1:
+        tabular_form()
+    else:
+        analytical_form()
